1ff7aa010bdeb624f58dac643936692288c68c76
yyyyds1 <1522849494@qq.com>
2024-08-07 23:42:28 +0800

    data_channel

--- include/iLive_data_channel.h
+++ include/iLive_data_channel.h
@@ -1,5 +1,20 @@
 #ifndef ILIVE_DATA_CHANNEL_H
 #define ILIVE_DATA_CHANNEL_H
 
+#include "iLive_webrtc_base.h"
+class RTCDataChannel;
+
+class iLiveDataChannelObserver{
+
+};
+
+class iLiveDataChannel{
+    public:
+        iLiveDataChannel(){}
+        void CreateDataChannel();
+        void DataChannelSend();
+        void DataChannelClose();
+        RTCDataChannel* DataChannelForId();
+};
 
 #endif
--- include/iLive_webrtc_base.h
+++ include/iLive_webrtc_base.h
@@ -1,5 +1,14 @@
-#ifndef ILIVE_RTC_AUDIO_DEVICE_H
-#define ILIVE_RTC_AUDIO_DEVICE_H
+#ifndef ILIVE_WEBRTC_BASE_H
+#define ILIVE_WEBRTC_BASE_H
 
+#include "iLive_video_renderer.h"
+#include "iLive_peerconnection.h"
+#include "iLive_media_stream.h"
+#include "iLive_data_channel.h"
+
+class iLiveWebRTCBase{
+    public:
+        friend class iLiveMediaStream;
+};
 
 #endif
